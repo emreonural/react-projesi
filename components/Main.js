2e553b228ca34357d4db14ecc6e7a0a845ab03b0
emreonural <88148421+emreonural@users.noreply.github.com>
2022-02-25 21:02:33 +0300

    Add files via upload

--- components/Main.js
+++ components/Main.js
@@ -11,7 +11,7 @@ class Main extends Component {
     constructor(props){
         super(props);
 
-        this.state= {
+        this.state = {
             windowH: 0,
             page: 0,
             touchxS: 0,
