e0388c787c0222e51d74465d6d057825ef425ac3
emreonural <88148421+emreonural@users.noreply.github.com>
2022-02-28 22:52:52 +0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -35,10 +35,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
+          <meta name="*viewport*" content="*width=device-width*, initial-scale=1, maximum-scale=1" />
           <title>ixirpad</title>
         </Head>
         <Component {...pageProps} />
